e104f4933479271c5b1b9d5c82dde8efefa10e5d
charliyang <charliyang@outlook.com>
2020-06-30 17:37:23 +0800

    初始化Redux项目

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
 import './App.css';
-import MyRCForm from './pages/MyRCForm';
-// import RCFromPage from './pages/RC-FormPage';
-// import ReactRouterPage from './pages/ReactRouterPage';
-// import ReactReduxPage from './pages/ReactReduxPage';
-// import HomePage from './pages/HomePage';
-// import ReduxPage from './pages/ReduxPage';
-// import ClassComponent from './pages/ClassComponent';
-// import FunctionComponent from './pages/FunctionComponent';
+import ReduxPage from './pages/ReduxPage';
 
 
 function App() {
   return (
     <div className="App"> 
-      {/* <ClassComponent /> */}
-      {/* <FunctionComponent/> */}
-      {/* <HomePage/> */}
-      {/* <ReduxPage/> */}
-      {/* <ReactReduxPage/> */}
-      {/* <ReactRouterPage/> */}
-      {/* <RCFromPage/> */}
-      <MyRCForm/>
+      <ReduxPage/>
     </div>
   );
 }
--- src/pages/ReduxPage.js
+++ src/pages/ReduxPage.js
@@ -1,23 +1,9 @@
-import React, { Component } from 'react'
-import store from '../store'
+import React, { Component } from 'react';
 
-class ReduxPage extends Component {
-    componentDidMount() {
-        store.subscribe(() => {
-            this.forceUpdate()
-        })
-    }
-    render() {
-        console.log('store', store);
-        
-        return (
-            <div>
-                <h1>这是一个Redux页面</h1>
-                <p>{store.getState()}</p>
-                <button onClick={() => store.dispatch({type: 'ADD'})}>add</button>
-            </div>
-        )
-    }
+export default class ReduxPage extends Component {
+  render() {
+    return (
+      <div> ReduxPage </div>
+    );
+  }
 }
-
-export default ReduxPage
