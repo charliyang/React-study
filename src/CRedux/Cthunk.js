c7f1d8184111f750ac4376468e8b3447b7371dc1
charliyang <charliyang@outlook.com>
2020-07-02 10:41:12 +0800

    Redux-React/Hooks  init

--- src/CRedux/Cthunk.js
+++
@@ -1,8 +0,0 @@
-export default function Cthunk({dispatch, getState}) {
-    return next => action => {
-        if (typeof action === "function") {
-            return action(dispatch, getState);
-      }
-          return next(action);
-    }
-}
