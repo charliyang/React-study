3ce3a126191255fa352be76db332b5255426d249
charliyang <charliyang@outlook.com>
2020-07-11 09:59:18 +0800

    06  init umi

--- src/action/const.js
+++
@@ -1,7 +0,0 @@
-export const REQUEST = "REQUEST";
-export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
-export const LOGIN_FAILURE = "LOGIN_FAILURE";
-export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
-export const LOGOUT_FAILURE = "LOGOUT_FAILURE";
-export const LOGIN_SAGA = "LOGIN_SAGA";
-export const LOGOUT_SAGA = "LOGOUT_SAGA"
--- src/action/rootSaga.js
+++
@@ -1,8 +0,0 @@
-import { all } from 'redux-saga/effects'
-
-import loginSaga from './loginSaga'
-import logoutSaga from './loginOutSaga'
-
-export default function* rootSaga() {
-    yield all ([logoutSaga(), loginSaga()])
-}
